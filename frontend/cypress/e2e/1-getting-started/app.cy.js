c255ddf00171f9fd388bc4c70eaf9b389852621a
AnassEREKYSY <ereanass@gmail.com>
2025-01-21 15:44:52 +0100

    corrected cypress test  part-2

--- frontend/cypress/e2e/1-getting-started/app.cy.js
+++ frontend/cypress/e2e/1-getting-started/app.cy.js
@@ -1,6 +1,6 @@
 describe('Frontend Tests', () => {
   it('should load the frontend and display the title', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.contains('Welcome to Simple Microservices');
   });
 });
